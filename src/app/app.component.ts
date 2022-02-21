546cfb1922e30a1304e8b2b5c3439fc7efc52a67
Zakamsky <alex.zakamsky@gmail.com>
2022-02-21 22:57:14 +0100

    add post component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface  Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-basics';
+
+  posts: Post[] = [
+    {
+      title: "Want to learn Angular",
+      text: "i am on my way!",
+      id: 1,
+    },
+    {
+      title: "stile Want to learn Angular",
+      text: "yet another lesson",
+      id: 2,
+    }
+  ]
 }
